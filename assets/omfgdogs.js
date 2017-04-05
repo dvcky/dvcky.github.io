a2bbc8ec08fec1dff69d16da1ece0dcc91b22cff
Cole Backman <colejbackman@gmail.com>
2017-04-04 23:50:29 -0500

    Organized website

--- assets/omfgdogs.js
+++
@@ -1,9 +0,0 @@
-var omfgdogs = document.getElementById(assets/omfgdogs.mp3);
-
-omfgdogs();
-
-function omfgdogs() {
-    omfgdogs.autoplay = true;
-    omfgdogs.loop = true;
-    omfgdogs.load();
-}
