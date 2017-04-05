6d49296df102f413f0273c971647c25167de623e
Cole Backman <colejbackman@gmail.com>
2017-04-04 22:22:54 -0500

    Fixed background audio.

--- assets/audioLoop.js
+++
@@ -1,6 +0,0 @@
-function audioLoop(url) {
-	var audioLoop = document.getElementById(url);
-	audio.loop=true;
-	audio.load();
-	audio.play();
-}
---
+++ assets/omfgdogs.js
@@ -0,0 +1,5 @@
+var audioLoop = document.getElementById(assets/omfgdogs.mp3);
+
+audio.autoplay = true;
+audio.loop = true;
+audio.load();
