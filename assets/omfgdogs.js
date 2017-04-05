daf1f0ec801d4cd91cf5094894ec4e25da8962cb
Cole Backman <colejbackman@gmail.com>
2017-04-04 22:25:08 -0500

    Update omfgdogs.js

--- assets/omfgdogs.js
+++ assets/omfgdogs.js
@@ -1,5 +1,5 @@
-var audioLoop = document.getElementById(assets/omfgdogs.mp3);
+var omfgdogs = document.getElementById(assets/omfgdogs.mp3);
 
-audio.autoplay = true;
-audio.loop = true;
-audio.load();
+omfgdogs.autoplay = true;
+omfgdogs.loop = true;
+omfgdogs.load();
