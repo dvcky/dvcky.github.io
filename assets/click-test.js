415af25a7a6ab5897aa1a882271ff64b899f3c9e
Cole Backman <colejbackman@gmail.com>
2017-05-05 12:53:46 -0500

    Update click-test.js

--- assets/click-test.js
+++ assets/click-test.js
@@ -3,3 +3,6 @@ function onClick() {
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
 }
+sleep(10000);
+var cps = clicks / 10;
+document.getElementById("cps").innerHTML = cps;
