e1f9c2b5b7b4d268eb7306944cb93e0e62308a2f
student <student>
2017-05-09 08:08:50 -0500

    Working on a timer for the CPS test.

--- assets/click-test.js
+++ assets/click-test.js
@@ -1,8 +1,35 @@
 var clicks = 0;
+var millisec = 0;
+var seconds = 0;
+var timer;
+
 function onClick() {
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
 }
-sleep(10000);
-var cps = clicks / 10;
-document.getElementById("cps").innerHTML = cps;
+
+function display(){
+
+  if (millisec>=9){
+     millisec=0
+     seconds+=1
+  }
+  else
+     millisec+=1
+     document.getElementById("stopwatch").innerHTML = seconds + "." + millisec;
+     timer = setTimeout("display()",100);
+  }
+
+function starttimer() {
+	
+  if (timer > 0) {
+	return;
+  }
+  display();	
+}
+
+function resettimer() {
+	stoptimer();
+	millisec = 0;
+	seconds = 0;
+}
