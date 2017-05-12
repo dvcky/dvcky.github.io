c795b0885de2a627c82660f4598b689418510809
student <student@ID-314-25-61455.ws.moundsviewschools.org>
2017-05-12 08:20:53 -0500

    .

--- assets/click-test.js
+++ assets/click-test.js
@@ -1,39 +1,48 @@
+var centiseconds = 0;
 var clicks = 0;
-var cps;
+var cps = 0;
+var deciseconds = 0;
 var isDone = false;
-var millisec = 0;
 var seconds = 0;
-var timer;
-
-function onClick() {
-    clicks += 1;
-    document.getElementById("clicks").innerHTML = clicks;
-}
+var stopwatch = 0;
 
 function display(){
 
-  if (seconds>=10) {
-     isDone = true
-  }
-  if (isDone) {
-	cps = clicks / 10
-	document.getElementById("results").innerHTML = "Your average CPS is " + cps;
-    document.getElementById("status").remove();
-  }
-  if (millisec>=9){
-     millisec=0
-     seconds+=1
-  }
-  else
-     millisec+=1
-     document.getElementById("stopwatch").innerHTML = seconds + "." + millisec;
-     timer = setTimeout("display()",100);
-  }
+    if (seconds>=10) {
+        isDone = true
+    }
+    if (isDone) {
+        cps = clicks / 10
+        document.getElementById("status").remove();
+        document.getElementById("results").innerHTML = "Your average CPS is " + cps;
+    }
 
-function starttimer() {
-  isDone = false
-  if (timer > 0) {
-	return;
-  }
-  display();	
+    if (deciseconds >= 9) {
+        deciseconds = 0
+        seconds += 1
+    } else if (centiseconds >= 9){
+        centiseconds = 0
+        deciseconds += 1
+    } else
+        centiseconds+=1
+        document.getElementById("stopwatch").innerHTML = seconds + "." + deciseconds + centiseconds;
+        stopwatch = setTimeout("display()",10);
+
+}
+
+function onClick() {
+	
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+
+}
+
+function onStart() {
+
+    if (stopwatch > 0) {
+        return;
+	}
+
+    display();
+
 }
