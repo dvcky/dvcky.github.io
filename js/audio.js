b4ebdccc5443c9195e74c883ba843aced520dc19
Cole Backman <colejbackman@gmail.com>
2017-05-18 12:54:03 -0500

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,3 +1,10 @@
+/* audio src */
 var audio = new Audio('../audio/Resonance%20-%20Loneliness%20%5Bmp3clan%5D.mp3');
-audio.volume = 0.2;
+
+/* audio settings */
+audio.autoplay = true;
+audio.loop = true;
+audio.volume = 0.1;
+
+/* audio play */
 audio.play();
