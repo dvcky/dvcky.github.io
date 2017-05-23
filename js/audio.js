524bd9fab5554fbfe28ea2139844f97e5f8db161
Cole Backman <colejbackman@gmail.com>
2017-05-22 19:35:00 -0500

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,10 +1,21 @@
 /* audio src */
 var audio = new Audio('../audio/Resonance%20-%20Loneliness%20%5Bmp3clan%5D.mp3');
+var load = document.cookie;
 
 /* audio settings */
 audio.autoplay = true;
+audio.currentTime = load;
 audio.loop = true;
 audio.volume = 0.1;
 
 /* audio play */
 audio.play();
+
+/* audio resume */
+load()
+function load() {
+    setInterval(function() {
+        /* loop */
+        document.cookie = audio.currentTime;
+    }, 1);
+}
