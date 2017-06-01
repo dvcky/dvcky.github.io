7b74765a4afc135807c224ed6c61fce459a39608
Cole Backman <colejbackman@gmail.com>
2017-06-01 12:28:23 -0500

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,24 +1,22 @@
 var ap = new APlayer({
-    element: document.getElementById('player'),                       // Optional, player element
-    narrow: true,                                                     // Optional, narrow style
-    autoplay: true,                                                    // Optional, autoplay song(s), not supported by mobile browsers
-    showlrc: false,                                                        // Optional, show lrc, can be 0, 1, 2, see: ###With lrc
-    mutex: true,                                                       // Optional, pause other players when this player playing
-    theme: '#e6d0b2',                                                  // Optional, theme color, default: #b7daff
-    mode: 'random',                                                    // Optional, play mode, can be `random` `single` `circulation`(loop) `order`(no loop), default: `circulation`
-    preload: 'auto',                                               // Optional, the way to load music, can be 'none' 'metadata' 'auto', default: 'auto'
-    listmaxheight: '513px',                                             // Optional, max height of play list
-    music: {                                                           // Required, music info, see: ###With playlist
-        title: 'Loneliness',                                          // Required, music title
-        author: 'Resonance',                          // Required, music author
-        url: 'audio/Resonance%20-%20Loneliness.mp3',                   // Required, music url
-        pic: 'images/Resonance%20-%20Loneliness.jpg',  // Optional, music picture
+    element: document.getElementById('player'),
+    narrow: false,
+    autoplay: false,
+    showlrc: false,
+    mutex: true,
+    theme: '#ad7a86',
+    mode: 'random',
+    music: {
+        title: 'Loneliness',
+        author: 'Resonance',
+        url: 'audio/Resonance%20-%20Loneliness.mp3', 
+        pic: 'images/Resonance%20-%20Loneliness.jpg',
     },
     
-    {                                                           // Required, music info, see: ###With playlist
-        title: 'Fantastic',                                          // Required, music title
-        author: 'Sub.Sound',                          // Required, music author
-        url: 'audio/Sub.Sound%20-%20Fantastic.mp3',                   // Required, music url
-        pic: 'images/Sub.Sound%20-%20Fantastic.jpg',  // Optional, music picture
+    {
+        title: 'Fantastic',
+        author: 'Sub.Sound',
+        url: 'audio/Sub.Sound%20-%20Fantastic.mp3',
+        pic: 'images/Sub.Sound%20-%20Fantastic.jpg',
     }
 });
