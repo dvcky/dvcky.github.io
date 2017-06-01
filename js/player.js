24d5d2826c65539100c63b984856d03e361227c9
Cole Backman <colejbackman@gmail.com>
2017-06-01 12:38:15 -0500

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,22 +1,29 @@
 var ap4 = new APlayer({
-    element: document.getElementById('player'),
+    element: document.getElementById('player4'),
     narrow: false,
     autoplay: false,
     showlrc: false,
     mutex: true,
     theme: '#ad7a86',
     mode: 'random',
-    music: {
-        title: 'Loneliness',
-        author: 'Resonance',
-        url: 'audio/Resonance%20-%20Loneliness.mp3', 
-        pic: 'images/Resonance%20-%20Loneliness.jpg',
-    },
-    
-    {
-        title: 'Fantastic',
-        author: 'Sub.Sound',
-        url: 'audio/Sub.Sound%20-%20Fantastic.mp3',
-        pic: 'images/Sub.Sound%20-%20Fantastic.jpg',
-    }
+    music: [
+        {
+            title: 'Loneliness',
+            author: 'Resonance',
+            url: 'audio/Resonance%20-%20Loneliness.mp3',
+            pic: 'images/Resonance%20-%20Loneliness.jpg'
+        },
+        {
+            title: 'Fantastic',
+            author: 'Sub.Sound',
+            url: 'audio/Sub.Sound%20-%20Fantastic.mp3',
+            pic: 'images/Sub.Sound%20-%20Fantastic.jpg'
+        },
+        {
+            title: '回レ！雪月花',
+            author: '小倉唯',
+            url: 'http://devtest.qiniudn.com/回レ！雪月花.mp3',
+            pic: 'http://devtest.qiniudn.com/回レ！雪月花.jpg'
+        }
+    ]
 });
