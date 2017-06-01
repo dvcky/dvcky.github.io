4bff2d4812ada2db834cd07763ae34b4aacb9eeb
student <student@ID-314-25-61455.ws.moundsviewschools.org>
2017-06-01 08:16:01 -0500

    Added APlayer.

--- js/player.js
+++ js/player.js
@@ -1,17 +1,17 @@
-var audio = new Audio('audio/Resonance%20-%20Loneliness.mp3');
-
-function changeImage() {
-    if(document.getElementById('play').src == 'pause.jpg') {
-        document.getElementById('play').src = 'play.jpg';
-    } else if(document.getElementById('play').src == 'play.jpg') {
-        document.getElementById('play').src = 'pause.jpg';
+var ap = new APlayer({
+    element: document.getElementById('player'),                       // Optional, player element
+    narrow: false,                                                     // Optional, narrow style
+    autoplay: true,                                                    // Optional, autoplay song(s), not supported by mobile browsers
+    showlrc: 0,                                                        // Optional, show lrc, can be 0, 1, 2, see: ###With lrc
+    mutex: true,                                                       // Optional, pause other players when this player playing
+    theme: '#e6d0b2',                                                  // Optional, theme color, default: #b7daff
+    mode: 'random',                                                    // Optional, play mode, can be `random` `single` `circulation`(loop) `order`(no loop), default: `circulation`
+    preload: 'auto',                                               // Optional, the way to load music, can be 'none' 'metadata' 'auto', default: 'auto'
+    listmaxheight: '513px',                                             // Optional, max height of play list
+    music: {                                                           // Required, music info, see: ###With playlist
+        title: 'Loneliness',                                          // Required, music title
+        author: 'Resonance',                          // Required, music author
+        url: 'audio/Resonance%20-%20Loneliness.mp3',                   // Required, music url
+        pic: 'images/Resonance%20-%20Loneliness.jpg',  // Optional, music picture
     }
-}
-
-function play() { 
-    audio.play(); 
-} 
-
-function pause() { 
-    audio.pause(); 
-}
+});
