ae7298e1b75318eb6d83f6ce15917657feaa0960
Cole Backman <colejbackman@gmail.com>
2017-08-05 15:27:51 -0500

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -2,6 +2,8 @@ var isMobile;
 function checkMobile() {
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         isMobile = true;
+        document.getElementById("content").remove();
+        document.getElementById("menu").remove();
     } else {
         isMobile = false;
         document.getElementById("mobile").remove();
