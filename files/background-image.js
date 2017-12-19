dd02b44b04d6abeb3940634679b17a430200ee36
Cole <colejbackman@gmail.com>
2017-12-19 09:12:14 -0600

    Update background-image.js

--- files/background-image.js
+++ files/background-image.js
@@ -1,5 +1,12 @@
-var x = event.clientX;
-var y = event.clientY;
-var coords = x + " " + y;
-
-document.getElementsByClassName("mouse").innerHTML = coords;
+$(document).ready(function() {
+    var movementStrength = 25;
+    var height = movementStrength / $(window).height();
+    var width = movementStrength / $(window).width();
+    $("#header").mousemove(function(e){
+        var pageX = e.pageX - ($(window).width() / 2);
+        var pageY = e.pageY - ($(window).height() / 2);
+        var newvalueX = width * pageX * -1 - 25;
+        var newvalueY = height * pageY * -1 - 50;
+        $('.background-image').css("background-position", newvalueX+"px     "+newvalueY+"px");
+});
+});
