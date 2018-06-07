de84d004df58bbac2450cc87cd0fd82976a6b469
Cole <colejbackman@gmail.com>
2018-06-07 18:48:09 -0500

    Update elootgg.js

--- elootgg.js
+++ elootgg.js
@@ -1,8 +1,3 @@
-var script = document.createElement('script');
-script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-
 $.getJSON("//api.eloot.gg/account/get", function (data) {
     console.log(data);
 });
