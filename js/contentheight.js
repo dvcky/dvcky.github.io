1092de2c323fe7dc0f8f0cc412c4de0787b934ed
Ducky (Zyp) <15352128+dduckyy@users.noreply.github.com>
2018-11-20 17:22:03 -0600

    Update contentheight.js

--- js/contentheight.js
+++ js/contentheight.js
@@ -2,7 +2,6 @@ function detectContentHeight() {
     setInterval(function(){
         var topOfContent = $('.content').offset().top;
         var bottomOfWindow = $(document).height();
-        
         var contentHeight = bottomOfWindow - topOfContent;
         
         $('.content').css('height', contentHeight);
