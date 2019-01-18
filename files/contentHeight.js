43b74eba7a5c8adaa7279af19a59b5940aca742f
Ducky (Zyp) <15352128+dduckyy@users.noreply.github.com>
2019-01-17 21:40:54 -0600

    major remake

---
+++ files/contentHeight.js
@@ -0,0 +1,12 @@
+function detectContentHeight() {
+    setInterval(
+        function(){          
+            var topOfContent = $('.content').offset().top;
+            var bottomOfWindow = $(document).height();
+
+            var contentHeight = bottomOfWindow - topOfContent;
+        
+            $('.content').css('height', contentHeight);
+        }, 
+    100);
+}
